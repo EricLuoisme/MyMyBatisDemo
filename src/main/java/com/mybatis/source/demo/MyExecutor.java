eda36d1956c1d5e7ca5a7f3c02486f75158a8dc8
lykismekk <lykismekk@outlook.com>
2021-10-30 20:20:58 +0800

    initial commit

---
+++ src/main/java/com/mybatis/source/demo/MyExecutor.java
@@ -0,0 +1,4 @@
+package com.mybatis.source.demo;
+
+public class MyExecutor {
+}
---
+++ src/main/java/com/mybatis/source/demo/entity/Department.java
@@ -0,0 +1,26 @@
+package com.mybatis.source.demo.entity;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+// 使用lombok实现链式constructor
+@Accessors(chain = true)
+public class Department implements Serializable {
+
+    // 这里必须与数据库中字段名完全一致, 才能绑定上, 否则需要在xml文件中重新设置resultMap进行映射
+    private Long dept_no;
+
+    private String dept_name;
+
+    private String db_source;
+
+    public Department(String deptName) {
+        this.dept_name = dept_name;
+    }
+}
